b188c0ab72fe2c6070e7c267d7282f82e68cb1e1
Olga <gudovaolya@gmail.com>
2016-09-28 14:17:16 +0600

    add last commits of the previous repository

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,23 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :correct_user, only: [:edit]
+
   def index
     @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -14,12 +25,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.delete
+      redirect_to root_path
+    else
+      render @post
+    end
   end
 
   private
@@ -27,5 +51,10 @@ class PostsController < ApplicationController
   def post_params
       params.require(:post).permit(:title, :body, :post_category_id, :organization_id, :user_id)
 
+  end
+
+  def correct_user
+    @post = Post.find(params[:id])
+    redirect_to(@post) unless current_user == @post.user
   end
 end
